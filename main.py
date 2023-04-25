9ac025764254658b8acd6de3accef3d7e9529e9a
ankit <ankit.rr528@gmail.com>
2023-04-25 17:33:59 +0530

    added evaluation metric and displayed the accuracy

--- main.py
+++ main.py
@@ -1,63 +1,63 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, mean_squared_error
 
-# Generate a random complex number
-z = np.random.rand() + np.random.rand() * 1j
 
-# Generate a random arithmetic operation
-operation = np.random.choice(["+", "-", "x", "/"])
-
-X = []
-y = []
-
-for i in range(1000):
-    # Generate two random complex numbers
-    z1 = np.random.rand() + np.random.rand() * 1j
-    z2 = np.random.rand() + np.random.rand() * 1j
-
-    # Generate a random arithmetic operation
+def generateDataset():
     operation = np.random.choice(["+", "-", "x", "/"])
 
-    # Compute the result of the arithmetic operation
-    if operation == "+":
-        result = z1 + z2
-        opcode = 0
-    elif operation == "-":
-        result = z1 - z2
-        opcode = 1
-    elif operation == "x":
-        result = z1 * z2
-        opcode = 2
-    elif operation == "/":
-        result = z1 / z2
-        opcode = 3
+    X = []
+    y = []
 
-    # Append the input and output to the dataset
-    X.append([z1.real, z1.imag, z2.real, z2.imag, opcode])
-    y.append([result.real, result.imag])
+    for _ in range(10000):
+        z1 = np.random.rand() + np.random.rand() * 1j
+        z2 = np.random.rand() + np.random.rand() * 1j
 
-X = np.array(X)
-y = np.array(y)
+        operation = np.random.choice(["+", "-", "x", "/"])
 
-# Create a neural network
+        if operation == "+":
+            result = z1 + z2
+            opcode = 0
+        elif operation == "-":
+            result = z1 - z2
+            opcode = 1
+        elif operation == "x":
+            result = z1 * z2
+            opcode = 2
+        elif operation == "/":
+            result = z1 / z2
+            opcode = 3
+
+        X.append([z1.real, z1.imag, z2.real, z2.imag, opcode])
+        y.append([result.real, result.imag])
+
+    X = np.array(X)
+    y = np.array(y)
+
+    return X, y
+
+
+X, y = generateDataset()
+np.savetxt("dataset.csv", np.concatenate((X, y), axis=1), delimiter=",")
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 model = Sequential()
 model.add(Dense(64, input_dim=5, activation="relu"))
 model.add(Dense(64, activation="relu"))
 model.add(Dense(2, activation="linear"))
 
-# Compile the neural network
 model.compile(loss="mse", optimizer="adam")
 
-# Train the neural network
-model.fit(X, y, epochs=100, batch_size=32)
+model.fit(X_train, y_train, epochs=100, batch_size=32)
 
-# Generate a new set of random complex numbers
-z1 = np.random.rand() + np.random.rand() * 1j
-z2 = np.random.rand() + np.random.rand() * 1j
+score = model.evaluate(X_test, y_test, batch_size=32)
+print("Test loss:", score)
 
-# Predict the result using the neural network
-result = model.predict(np.array([[z1.real, z1.imag, z2.real, z2.imag, 0]]))
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
 
-# Print the result
-print(f"{z1} + {z2} = {result[0][0]} + {result[0][1]}i")
+print("Mean squared error:", mse)
